22ac7a81c000aa432a59f415083814bd20f365db
Shaffie <146901570+MasterPek@users.noreply.github.com>
2025-05-29 11:25:32 +0800

    Add .next/static/chunks/src_app_projects_page_tsx_14654257._.js 1748489131790

---
+++ .next/static/chunks/src_app_projects_page_tsx_14654257._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@mui_8f68e31b._.js",
+  "static/chunks/src_app_projects_page_tsx_a5772c6c._.js"
+],
+    source: "dynamic"
+});
