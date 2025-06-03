a8209fac77076c0d75ccd9cf243726c092afa9cd
Shaffie <146901570+MasterPek@users.noreply.github.com>
2025-06-03 10:55:37 +0800

    Add .next/static/chunks/src_app_embed_[token]_page_tsx_14654257._.js 1748919336447

---
+++ .next/static/chunks/src_app_embed_[token]_page_tsx_14654257._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/423b6_three_build_three_core_7f8c5658.js",
+  "static/chunks/423b6_three_build_three_module_4d94dd37.js",
+  "static/chunks/node_modules_@photo-sphere-viewer_core_index_module_8c52a2b2.js",
+  "static/chunks/node_modules_@mui_material_CircularProgress_5528ac58._.js",
+  "static/chunks/src_d22c48d6._.js",
+  "static/chunks/node_modules_@photo-sphere-viewer_core_index_61a46108.css"
+],
+    source: "dynamic"
+});
